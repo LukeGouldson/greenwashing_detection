89058a43b198fa5b67964c3e07d10324b66b1cf8
Luke Gouldson <gouldsonluke@gmail.com>
2025-07-17 13:24:58 +0100

    Updated preprocessing.py and fine_tuning.ipynb after errors

--- preprocessing.py
+++ preprocessing.py
@@ -4,14 +4,11 @@ from datasets import Dataset, DatasetDict
 pd.set_option('display.max_columns', None)
 
 
+# This function explodes the evidences section of climate-fever such that we produce 5 claim-evidence pairs for each claim
+# The dataset therefore becomes x5 bigger
 def climate_fever_to_claim_evidence_pairs(df):
-    print("DF\n", df.head())
 
     exploded_df = df.explode("evidences", ignore_index=True)
-    # df_exploded = df_exploded.drop(columns=["evidences"]).reset_index(drop=True)
-
-    print("\nEXPLODED\n", exploded_df.head())
-    print(f"\nColumns: {exploded_df.columns}")
 
     evidences = pd.json_normalize(exploded_df["evidences"])
 
@@ -20,18 +17,8 @@ def climate_fever_to_claim_evidence_pairs(df):
         evidences[["evidence_id", "evidence_label", "evidence", "entropy"]]
     ], axis=1)
 
-    print("\nPREPROCESSED\n", preprocessed_df.head())
-    print(f"\nColumns: {preprocessed_df.columns}\n")
-    print(preprocessed_df.loc[1])
+    # Outputs a dataframe with columns: ['claim_id', 'claim', 'evidence_id', 'evidence_label', 'evidence', 'entropy']
+    return preprocessed_df
 
-    dataset = Dataset.from_pandas(preprocessed_df)
-    return dataset
-
-
-
-df = pd.read_json("/home/lukeg/Documents/VS_code/climate_project/lxg406/data/climate_fever/climate-fever-dataset-sample.jsonl", lines=True)
-ds = climate_fever_to_claim_evidence_pairs(df=df)
-print(ds[4])
-print(ds[3])
-print(ds[9])
-print(ds[77])
+# df = pd.read_json("/home/lukeg/Documents/VS_code/climate_project/lxg406/data/climate_fever/climate-fever-dataset-sample.jsonl", lines=True)
+# ds = climate_fever_to_claim_evidence_pairs(df=df)
