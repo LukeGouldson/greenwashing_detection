9051b83e31e8b557a80178425aeaf6bf8e3fe372
Luke Gouldson <gouldsonluke@gmail.com>
2025-08-14 18:31:20 +0100

    Fully updated remote

--- unsubstantiated_claims_classification/fine_tuning/preprocessing.py
+++ unsubstantiated_claims_classification/fine_tuning/preprocessing.py
@@ -11,6 +11,7 @@ def climate_fever_to_claim_evidence_pairs(df):
     exploded_df = df.explode("evidences", ignore_index=True)
 
     evidences = pd.json_normalize(exploded_df["evidences"])
+    print("EVIDENCES", evidences)
 
     preprocessed_df = pd.concat([
         exploded_df[["claim_id", "claim"]].reset_index(drop=True),
@@ -20,5 +21,5 @@ def climate_fever_to_claim_evidence_pairs(df):
     # Outputs a dataframe with columns: ['claim_id', 'claim', 'evidence_id', 'evidence_label', 'evidence', 'entropy']
     return preprocessed_df
 
-# df = pd.read_json("/home/lukeg/Documents/VS_code/climate_project/lxg406/data/climate_fever/climate-fever-dataset-sample.jsonl", lines=True)
-# ds = climate_fever_to_claim_evidence_pairs(df=df)
+df = pd.read_json("/home/lukeg/Documents/VS_code/climate_project/lxg406/data/climate_fever/climate-fever-dataset-sample.jsonl", lines=True)
+ds = climate_fever_to_claim_evidence_pairs(df=df)
